d360d0c36279957e5f1312f5b62eebc7c8bc338d
Florent <florent@peterschmitt.fr>
2013-05-28 10:14:22 +0200

    rpminfo.py: docstring

--- rpminfo.py
+++ rpminfo.py
@@ -6,17 +6,23 @@ import os
 import rpm
 
 class RPMInfo:
-    def __init__():
-        pass
-
+    """
+    Retrieve informations from RPM package.
+    """
     @staticmethod
     def isa_rpm(f_rpm):
+        """
+        Ugly method to tell if it's a real RPM file.
+        """
         if os.path.isfile(f_rpm) and '.rpm' in f_rpm[-4:] and not os.path.islink(f_rpm):
             return True
         return False
 
     @staticmethod
     def get_info(f_rpm, tag):
+        """
+        Give info of file [f_rpm] and tag [rpm.TAG*]
+        """
         transaction = rpm.ts()
         fdno = os.open(f_rpm, os.O_RDONLY)
 
