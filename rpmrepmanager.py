f3c2623275b145139bda8f24b2a67d2654caf9cf
Florent Peterschmitt <florent@peterschmitt.fr>
2013-06-24 15:03:48 +0200

    help

--- rpmrepmanager.py
+++ rpmrepmanager.py
@@ -280,17 +280,50 @@ class RPMRepManager:
     @staticmethod
     def MakeNeededOptions():
         parser = optparse.OptionParser()
-        parser.add_option('--base', dest='base', help='base dir for everything')
-        parser.add_option('--version', dest='version', help='version of distro')
-        parser.add_option('--arch', dest='arch', help='architecture')
-        parser.add_option('--repo', dest='repo', default="", help='repository for symlinks to rpm')
-        parser.add_option('--fake', action='store_true', default=True, help='run fake remake')
-        parser.add_option('--real', action='store_true', default=False, help='run real remake')
-        parser.add_option('--unsigned', action='store_true', default=True, help='link unsigned packages')
-        parser.add_option('--verbose', action='store_true', default=False, help='if an action is performed, say it')
-        parser.add_option('--report', action='store_true', default=True, help='like verbose but makes a report of all actions')
-        parser.add_option('--cleanup', action='store_true', default=True, help='clean old versions/release of a package. dont touch signed packages unless --force-delete')
-        parser.add_option('--force-delete', action='store_true', default=False, help='force deletion of old packages, event if signed. use with CAUTION.')
-        parser.add_option('--wipe-repo', action='store_true', default=False, help='wipe repository instead of just remake missing/invalid symlinks to RPM')
-        parser.add_option('--wipe-all-old', action='store_true', default=False, help='default is to keep both latest unsigned and signed package. This option forces to delete all old packages. useful with --force-delete')
+        parser.add_option('--base', dest='base',
+                help='base dir for everything')
+        parser.add_option('--version', dest='version',
+                help='version of distro')
+        parser.add_option('--arch', dest='arch',
+                help='architecture')
+
+        parser.add_option('--repo', dest='repo',
+                default="",
+                help='name of the repository under base/www/version-arch/')
+
+        parser.add_option('--fake', action='store_true',
+                default=True,
+                help='run in fake mode, no action performed. this is default mode.')
+
+        parser.add_option('--real', action='store_true',
+                default=False,
+                help='run in real mode')
+
+        parser.add_option('--unsigned', action='store_true',
+                default=False,
+                help='build repository with unsigned packages. default to signed only')
+
+        parser.add_option('--verbose', action='store_true',
+                default=False,
+                help='if an action is performed, say it immediately')
+
+        parser.add_option('--report', action='store_true',
+                default=True,
+                help='like verbose but makes a report of all actions at the end. this is default')
+
+        parser.add_option('--cleanup', action='store_true',
+                default=True,
+                help='clean old versions/release of a package. dont touch signed packages unless --force-delete. this is default')
+
+        parser.add_option('--force-delete', action='store_true',
+                default=False,
+                help='force deletion of old packages only, event if signed (but old)')
+
+        parser.add_option('--wipe-repo', action='store_true',
+                default=False,
+                help='wipe repository instead of just remake missing/invalid symlinks to RPM')
+
+        parser.add_option('--wipe-all-old', action='store_true',
+                default=False,
+                help='default is to keep both latest unsigned and signed package. this option forces to *really* delete *all old* packages. useful only with --force-delete')
         return parser
